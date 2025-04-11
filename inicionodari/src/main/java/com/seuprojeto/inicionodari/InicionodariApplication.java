6b10efb0d42857208be1794355f2e29e70100abe
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2025-04-11 10:28:14 -0400

    v0.0

---
+++ inicionodari/src/main/java/com/seuprojeto/inicionodari/InicionodariApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.inicionodari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicionodariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicionodariApplication.class, args);
+	}
+
+}
---
+++ inicionodari/src/test/java/com/seuprojeto/inicionodari/InicionodariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.inicionodari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicionodariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
